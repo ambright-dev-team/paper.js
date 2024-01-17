db34135f3ab3e1ba573e73d830143ed1d09f0561
Raivo Laanemets <raivo@infdot.com>
2024-01-17 16:32:43 +0200

    Still building full paper

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -21,7 +21,7 @@ var gulp = require('gulp'),
 // Options to be used in Prepro.js preprocessing through the global __options
 // object, merged in with the options required above.
 var buildOptions = {
-    full: { paperScript: false },
+    full: { paperScript: true },
     core: { paperScript: false }
 };
 
